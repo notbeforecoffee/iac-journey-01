83475a484d19015e9d66e637c4b5e35f46d405ca
gcastill0 <gilberto@hashicorp.com>
2023-04-14 11:24:27 -0400

    Updated Terraform deployment code

--- terraform/ec2instances.tf
+++ terraform/ec2instances.tf
@@ -1,67 +1,49 @@
-# # Create a new instance of the latest Ubuntu on an EC2 instance,
-# # t2.micro node. If you are not sure of what you are trying to find,
-# # try this using the AWS command line:
-# #
-# #  aws ec2 describe-images --owners 099720109477 \
-# #    --filters "Name=name,Values=*hvm-ssd*bionic*18.04-amd64*" \
-# #    --query 'sort_by(Images, &CreationDate)[].Name'
-# #
-# # aws ec2 describe-images --owners 099720109477 \
-# #   --filters "Name=name,Values=*hvm-ssd*focal*20.04-amd64*" \
-# #   --query 'sort_by(Images, &CreationDate)[].Name'
+# Create a new instance of the latest Ubuntu on an EC2 instance,
+# t2.micro node. If you are not sure of what you are trying to find,
+# try this using the AWS command line:
+#
+#  aws ec2 describe-images --owners 099720109477 \
+#    --filters "Name=name,Values=*hvm-ssd*bionic*18.04-amd64*" \
+#    --query 'sort_by(Images, &CreationDate)[].Name'
+#
+# aws ec2 describe-images --owners 099720109477 \
+#   --filters "Name=name,Values=*hvm-ssd*focal*20.04-amd64*" \
+#   --query 'sort_by(Images, &CreationDate)[].Name'
 
-# data "aws_ami" "ubuntu" {
-#   most_recent = true
+data "aws_ami" "ubuntu" {
+  most_recent = true
 
-#   filter {
-#     name   = "name"
-#     values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-#   }
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
 
-#   filter {
-#     name   = "virtualization-type"
-#     values = ["hvm"]
-#   }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
 
-#   owners = ["099720109477"] # Canonical
-# }
+  owners = ["099720109477"] # Canonical
+}
 
-# data "aws_availability_zones" "available" {
-#   state = "available"
-# }
+data "template_file" "happy_animals" {
+  template = file("${path.module}/../bash/deploy-app.sh")
+}
 
-# resource "aws_instance" "app" {
-#   count                  = length(var.instance_names)
-#   ami                    = data.aws_ami.ubuntu.id
-#   instance_type          = "t2.micro"
-#   key_name               = aws_key_pair.ssh.key_name
-#   subnet_id              = aws_subnet.interrupt_public_subnet[count.index].id
-#   vpc_security_group_ids = [aws_security_group.interrupt_app.id]
-#   depends_on             = [aws_internet_gateway.interrupt_gw]
+resource "aws_instance" "app" {
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = "t2.micro"
+  key_name               = aws_key_pair.main.key_name
+  vpc_security_group_ids = [aws_security_group.interrupt_app.id]
+  user_data              = data.template_file.happy_animals.rendered
+  tags                   = merge({ "Name" = "tf-iac-demo" }, var.tags)
 
-#   // user_data = templatefile("./init-script.sh", { file_content = "version 1.0 - #${count.index}" })
+  lifecycle {
+    create_before_destroy = true
+  }
 
-#   tags = merge({ "Name" = var.instance_names[count.index] }, var.tags)
+}
 
-#   lifecycle {
-#     create_before_destroy = true
-#   }
-
-# }
-
-# resource "aws_instance" "bastion" {
-#   count                  = 1
-#   ami                    = data.aws_ami.ubuntu.id
-#   instance_type          = "t2.micro"
-#   key_name               = aws_key_pair.ssh.key_name
-#   subnet_id              = aws_subnet.interrupt_public_subnet[count.index].id
-#   vpc_security_group_ids = [aws_security_group.bastion.id]
-#   depends_on             = [aws_internet_gateway.interrupt_gw]
-
-#   tags = merge({ "Name" = "bastion" }, var.tags)
-
-#   lifecycle {
-#     create_before_destroy = true
-#   }
-
-# }
+output "public_url" {
+  value = "http://${aws_instance.app.public_ip}"
+}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,6 @@
+variable "prefix" {
+  default = "tf-iac-demo"
+}
 
 variable "region" {
   default = "us-east-2"
@@ -10,13 +13,6 @@ variable "private_subnet_cidr" {
 variable "public_subnet_cidr" {
   default = "10.3.0.0/32"
 }
-
-# variable "key_name" {
-#   default = "interrupt-tmp-key"
-# }
-
-# #variable "ssh_key" {
-# #}
 
 variable "tags" {
   type = map(any)
@@ -34,19 +30,3 @@ variable "tags" {
     TFE_Worspace = "null"
   }
 }
-
-# variable "instance_names" {
-#   type    = list(string)
-#   default = ["nginx1", "nginx2", "nginx3"]
-# }
-
-# variable "private_subnet" {
-#   type    = list(string)
-#   default = ["10.168.1.0/24", "10.168.2.0/24", "10.168.3.0/24"]
-
-# }
-
-# variable "public_subnet" {
-#   type    = list(string)
-#   default = ["10.168.101.0/24", "10.168.102.0/24", "10.168.103.0/24"]
-# }
