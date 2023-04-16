b99da8ac6641ada5dd7396a3d7829b7c34a665cb
gcastill0 <gilberto@hashicorp.com>
2023-04-16 12:20:30 -0400

    Adding slideshow pages

---
+++ docs/js/paths.js
@@ -0,0 +1,329 @@
+var jsondata = {
+    "about-this-exercise": [
+        {
+            "id": 0,
+            "class": "cell-span-5-5",
+            "splash_delay": 3,
+            "image": "headshot.svg"
+        },
+        {
+            "id": 2,
+            "class": "cell-span-6-5",
+            "splash_delay": 1,
+            "innerHTML": {
+                "template": "/html/templates/p01_t01.html",
+                "template_class": "cell-content-normal"
+            }
+        }
+    ],
+    "track-contents": [
+        {
+            "id": 0,
+            "class": "cell-span-3-2",
+            "splash_delay": 3,
+            "label": {
+                "text": "Manual deployment",
+                "class": "cell-label-top"
+            },
+            "boxtype": "cell-decorated",
+            "innerHTML": {
+                "template": "/html/templates/p04_t01.html",
+                "template_class": "cell-content-small"
+            }
+        },
+        {
+            "id": 14,
+            "class": "cell-span-3-2",
+            "splash_delay": 3,
+            "image": "page_01_hashi-container-3-2-1.svg"
+        },
+        {
+            "id": 15,
+            "class": "cell-span-3-2",
+            "splash_delay": 3,
+            "image": "page_01_hashi-container-3-2-2.svg"
+        },
+        {
+            "id": 16,
+            "class": "cell-span-3-2",
+            "splash_delay": 3,
+            "image": "page_01_hashi-container-3-2-3.svg"
+        },
+        {
+            "id": 17,
+            "class": "cell-span-3-2",
+            "splash_delay": 3,
+            "image": "page_01_hashi-container-3-2-4.svg"
+        },
+        {
+            "id": 4,
+            "class": "cell-span-3-2",
+            "splash_delay": 3,
+            "label": {
+                "text": "IAC with Terraform",
+                "class": "cell-label-top"
+            },
+            "boxtype": "cell-decorated",
+            "innerHTML": {
+                "template": "/html/templates/p04_t03.html",
+                "template_class": "cell-content-small"
+            }
+        },
+        {
+            "id": 21,
+            "class": "cell-span-3-2",
+            "splash_delay": 3,
+            "label": {
+                "text": "CloudFormation",
+                "class": "cell-label-top"
+            },
+            "boxtype": "cell-decorated",
+            "innerHTML": {
+                "template": "/html/templates/p04_t02.html",
+                "template_class": "cell-content-small"
+            }
+        },
+        {
+            "id": 25,
+            "class": "cell-span-3-2",
+            "splash_delay": 3,
+            "label": {
+                "text": "Terraform Modules",
+                "class": "cell-label-top"
+            },
+            "boxtype": "cell-decorated",
+            "innerHTML": {
+                "template": "/html/templates/p04_t04.html",
+                "template_class": "cell-content-small"
+            }
+        }
+    ],
+    "adoption-journey-individual": [
+        {
+            "id": 0,
+            "class": "cell-span-5-5",
+            "splash_delay": 1,
+            "innerHTML": {
+                "template": "/html/templates/p02_t01.html",
+                "template_class": "cell-content-normal"
+            }
+        },
+        {
+            "id": 7,
+            "class": "cell-span-5-5",
+            "splash_delay": 3,
+            "image": "adoption_journey_individual.svg"
+        }
+    ],
+    "adoption-journey-teams": [
+        {
+            "id": 0,
+            "class": "cell-span-5-5",
+            "splash_delay": 1,
+            "innerHTML": {
+                "template": "/html/templates/p02_t02.html",
+                "template_class": "cell-content-normal"
+            }
+        },
+        {
+            "id": 7,
+            "class": "cell-span-5-5",
+            "splash_delay": 3,
+            "image": "adoption_journey_teams.svg"
+        }
+    ],
+    "adoption-journey-multiple-teams": [
+        {
+            "id": 0,
+            "class": "cell-span-5-5",
+            "splash_delay": 1,
+            "innerHTML": {
+                "template": "/html/templates/p02_t03.html",
+                "template_class": "cell-content-normal"
+            }
+        },
+        {
+            "id": 7,
+            "class": "cell-span-5-5",
+            "splash_delay": 3,
+            "image": "adoption_journey_multiple_teams.svg"
+        }
+    ],
+    "industry-challenges": [
+        {
+            "id": 0,
+            "class": "cell-span-12-6",
+            "splash_delay": 1,
+            "innerHTML": {
+                "template": "/html/templates/p98_t01.html",
+                "template_class": "cell-content-normal"
+            }
+        }
+    ],
+    "about-tde": [
+        {
+            "id": 0,
+            "class": "cell-span-1-6",
+            "label": {
+                "text": "Attack vectors",
+                "class": "cell-label-left"
+            }
+        },
+        {
+            "id": 7,
+            "class": "cell-span-3-3",
+            "label": {
+                "text": "Unauthorized User",
+                "class": "cell-label-top"
+            },
+            "image": "page_02_hashi-container-3-3.svg",
+            "splash_delay": 3,
+            "modal": "modal1"
+        },
+        {
+            "id": 8,
+            "class": "cell-span-4-2",
+            "label": {
+                "text": "Unauthorized Access",
+                "class": "cell-label-bottom",
+                "color": "red"
+            },
+            "image": "page_02_hashi-container-4-2-1.svg",
+            "splash_delay": 6,
+            "modal": "modal2"
+        },
+        {
+            "id": 9,
+            "class": "cell-span-5-5",
+            "label": {
+                "text": "MS SQL Servers",
+                "class": "cell-label-top"
+            },
+            "image": "page_02_hashi-container-5-5.svg",
+            "splash_delay": 9,
+            "modal": "modal3"
+        },
+        {
+            "id": 10,
+            "class": "cell-span-4-2",
+            "label": {
+                "text": "Unencrypted Data",
+                "class": "cell-label-bottom",
+                "color": "red"
+            },
+            "image": "page_02_hashi-container-4-2-2.svg",
+            "splash_delay": 12,
+            "modal": "modal4"
+        }
+    ],
+    "manual-deployment": [
+        {
+            "id": 0,
+            "class": "cell-span-1-6",
+            "label": {
+                "text": "Manual deployment with AWS CLI",
+                "class": "cell-label-left"
+            }
+        },
+        {
+            "id": 7,
+            "class": "cell-span-12-5",
+            "splash_delay": 3,
+            "image": "aws_cli_deploy.svg"
+        }
+    ],
+    "why-use-terraform": [
+        {
+            "id": 0,
+            "class": "cell-span-5-5",
+            "splash_delay": 1,
+            "innerHTML": {
+                "template": "/html/templates/p06_t01.html",
+                "template_class": "cell-content-normal"
+            }
+        },
+        {
+            "id": 2,
+            "class": "cell-span-6-5",
+            "splash_delay": 3,
+            "image": "contract_testing.svg"
+        },
+    ],
+    "terraform-deployment": [
+        {
+            "id": 0,
+            "class": "cell-span-1-6",
+            "label": {
+                "text": "IaC with Terraform",
+                "class": "cell-label-left"
+            }
+        },
+        {
+            "id": 7,
+            "class": "cell-span-12-5",
+            "splash_delay": 3,
+            "image": "terraform_deploy_instance.svg"
+        }
+    ],
+    "lab-environment": [
+        {
+            "id": 0,
+            "class": "cell-span-1-6",
+            "label": {
+                "text": "Lab Environment",
+                "class": "cell-label-left"
+            }
+        },
+        {
+            "id": 7,
+            "class": "cell-span-12-5",
+            "splash_delay": 3,
+            "image": "full_environment.svg"
+        }
+    ],
+    "lab-access": [
+        {
+            "id": 0,
+            "class": "cell-span-1-6",
+            "label": {
+                "text": "Lab Access",
+                "class": "cell-label-left"
+            }
+        },
+        {
+            "id": 7,
+            "class": "cell-span-12-5",
+            "splash_delay": 3,
+            "image": "aws_access_layer.svg"
+        }
+    ],
+    "cicd-enablement": [
+        {
+            "id": 0,
+            "class": "cell-span-1-6",
+            "label": {
+                "text": "Compare Manual vs. automated",
+                "class": "cell-label-left"
+            }
+        },
+        {
+            "id": 7,
+            "class": "cell-span-12-5",
+            "splash_delay": 3,
+            "image": "enable_cicd.svg"
+        }
+    ],
+    "summarize-benefits": [
+        {
+            "id": 0,
+            "class": "cell-span-12-6",
+            "splash_delay": 1,
+            "innerHTML": {
+                "template": "/html/templates/p99_t01.html",
+                "template_class": "cell-content-normal"
+            }
+        }
+    ]
+};
+
+export { jsondata };
